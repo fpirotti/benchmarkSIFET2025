0dcd78cf0c1d847bc5f59db68a61e292125791c9
fpirotti <francesco.pirotti@unipd.it>
2025-05-10 09:36:31 +0000

    go

--- 00_processBenchmark.R
+++ 00_processBenchmark.R
@@ -1,8 +1,33 @@
 library(lidR)
+library(CloudGeometry)
+
 parallel::detectCores()
 
-ctg <- readLAScatalog("data/cloud_merged2.laz")
-ctg
+
+dGeom <- function(chunk, rgbmap) # user defined function
+{
+  las <- readALSLAS(chunk)                  # read the chunk
+  if (is.empty(las)) return(NULL)        # check if it actually contain points
+  browser()
+  nlasrgb <- CloudGeometry()  # apply computation of interest
+  return(nlasrgb) # output
+}
+
+file <- "data/cloud_merged2.laz"
+ctg <- readLAScatalog(file)
+rlas::writelax(file)
+las_file <- index_create(ctg)
 opt_chunk_size(ctg) <- 100
 opt_chunk_buffer(ctg) <- 3
+opt_output_files(ctg) <- paste0(tempdir(), "/{ID}_norm_rgb") # write to disk
+lidR::index(ctg)
+# Step 2: Define a bounding box for spatial filtering
+bbox <- matrix(c(288500, 5091400, 288550, 5091450), ncol = 2, byrow = TRUE)
+
+# Step 3: Apply spatial filter to the catalog
+las_subset <- catalog_apply(ctg, filter = bbox)
+
+output <- catalog_apply(ctg, dGeom ) # implement user-defined function using catalog_apply
+str(output)
+
 plot(ctg, chunk = TRUE)
--- readme.md
+++ readme.md
@@ -2,6 +2,18 @@
 
 “Classificazione di nuvole di punti da drone”
 
+## Obiettivi
+
+Suddividere i punti rilevati con sensore laser scanner L2 in parti omogenee (segmenti o oggetti), in base a criteri estratti dalle variabili dispoinibili.
+
 ## Materiale
 
 Il benchmark mette a disposizione diversi prodotti da rilievo con drone con camere RGB, multispettrali e LiDAR su una zona agricola
+
+## Metodi
+
+come colore, texture, forma o contesto spaziale.
+
+## Risultati
+
+I dati sono visibili nel visualizzatore online basato su Potree [QUI](https://www.cirgeo.unipd.it/pointclouds/sifetBenchmark2025/).
