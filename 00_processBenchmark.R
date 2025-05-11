7ce92b148de19c5e2b8fcca081fc1ceabe8d11ab
fpirotti <francesco.pirotti@unipd.it>
2025-05-11 06:51:24 +0000

    readme

--- 00_processBenchmark.R
+++ 00_processBenchmark.R
@@ -1,33 +1,69 @@
 library(lidR)
 library(CloudGeometry)
-
-parallel::detectCores()
-
+library(lasR)
 
 dGeom <- function(chunk, rgbmap) # user defined function
 {
-  las <- readALSLAS(chunk)                  # read the chunk
+  las <- readLAS(chunk)                  # read the chunk
   if (is.empty(las)) return(NULL)        # check if it actually contain points
-  browser()
-  nlasrgb <- CloudGeometry()  # apply computation of interest
-  return(nlasrgb) # output
+
+  lasG <- CloudGeometry::calcGF(las@data[,1:3],rk = 1, threads = 32)  # apply computation of interest
+  for(i in names(lasG)){
+    las <- lidR::add_lasattribute_manual(las, lasG[,i], name=i, desc=i, type="float")
+  }
+  lasG <- CloudGeometry::calcGF(las@data[,1:3],rk = 0.5, threads = 32)  # apply computation of interest
+  for(i in names(lasG)){
+    las <- lidR::add_lasattribute_manual(las, lasG[,i], name=i, desc=i, type="float")
+  }
+  return(las) # output
 }
 
 file <- "data/cloud_merged2.laz"
 ctg <- readLAScatalog(file)
-rlas::writelax(file)
-las_file <- index_create(ctg)
 opt_chunk_size(ctg) <- 100
 opt_chunk_buffer(ctg) <- 3
-opt_output_files(ctg) <- paste0(tempdir(), "/{ID}_norm_rgb") # write to disk
-lidR::index(ctg)
+opt_output_files(ctg) <- paste0("data/{ID}_geom")
+
+
+output <- catalog_apply(ctg, dGeom )
+writeLAS( output[[1]], "data/output.laz")
+
+# class = classify_with_csf(FALSE, 1 ,0.1, rigidness = 2) +
+#   write_las(ofile = "data/tempfiles/*.las"  ) +
+#   write_lax()
+#
+# tri  <- triangulate(filter = keep_ground())
+# dtm  <- rasterize(1, tri) # input is a triangulation stage
+# # norm <-  normalize() +  write_las(ofile = "data/tempfiles/norm*.las"  )
+# pipeline <- class + tri + dtm #+ norm
+# ans2 = exec(pipeline, on = ctg, progress = TRUE, ncores=64)
+#
+# ans2 <- list.files("data/tempfiles/")
+# ctg2 <- readLAScatalog(ans2)
+# opt_chunk_size(ctg2) <- 0
+# opt_chunk_buffer(ctg2) <- 3
+# opt_output_files(ctg) <- paste0("data/{ID}_")
+
+# chm1 <- rasterize(1, "max")
+# chm2 <- rasterize(1, "min", filter = "Classification == 2" )
+
+# ans <- exec(pipeline, on = ctg2,  with = list(progress = TRUE,  ncores=64))
+
+terra::writeRaster(ans2$rasterize, "data/dtm.tif", overwrite=T)
+# terra::writeRaster(ans[[1]], "data/max.tif", overwrite=T)
+# rlas::writelax(file)
+
+
+# lidR::index(ctg)
 # Step 2: Define a bounding box for spatial filtering
-bbox <- matrix(c(288500, 5091400, 288550, 5091450), ncol = 2, byrow = TRUE)
 
-# Step 3: Apply spatial filter to the catalog
-las_subset <- catalog_apply(ctg, filter = bbox)
+las <- lidR::clip_circle(ctg,  288900, 5091400, 10)
 
-output <- catalog_apply(ctg, dGeom ) # implement user-defined function using catalog_apply
-str(output)
+opt_output_files(las) <- ""
+opt_chunk_size(las) <- 0
+opt_chunk_buffer(las) <- 0
+
+output <- catalog_apply(las, dGeom )
+writeLAS( output[[1]], "data/output.laz")
 
 plot(ctg, chunk = TRUE)
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 # Benchmark SIFET 2025
 
-“Classificazione di nuvole di punti da drone”
+**“Classificazione di nuvole di punti da drone”**
+
+Francesco Pirotti, Enrico Magazzino
+
+*CIRGEO Centro Interdipartimentale di Ricerca di Geomatica / TESAF Dipartimento Territorio e Sistemi Agroforestali, Università di Padova*
 
 ## Obiettivi
 
@@ -14,6 +18,24 @@ Il benchmark mette a disposizione diversi prodotti da rilievo con drone con came
 
 come colore, texture, forma o contesto spaziale.
 
+Descrittori geometrici da intorno di 50 cm e 1 m estratti con 32 threads -
+
+![](images/clipboard-2581011368.png){width="373"}
+
+I descrittori sono stati scalati e trasformati rispetto alla loro mediana ed alla loro deviazione standard in quanto spesso non seguono una distribuzione nornale .
+The standard mathematical **symbol for the median** is:
+ 
+
+$$
+z = \frac{x - \tilde{x}}{\text{MAD}(x)}
+
+\quad \text{where} \quad \tilde{x} = \text{median}(x)
+$$
+
+ 
+
 ## Risultati
 
 I dati sono visibili nel visualizzatore online basato su Potree [QUI](https://www.cirgeo.unipd.it/pointclouds/sifetBenchmark2025/).
+
+La nuvola dopo segmentazione è disponibile QUI
