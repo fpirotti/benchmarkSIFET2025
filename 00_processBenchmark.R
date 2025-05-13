6c729da75e7e4734d7180a23cecef2afb8e65b95
fpirotti <francesco.pirotti@unipd.it>
2025-05-13 02:14:54 +0000

    hh

--- 00_processBenchmark.R
+++ 00_processBenchmark.R
@@ -1,6 +1,8 @@
-library(lidR)
-library(CloudGeometry)
+pacman::p_load("lidR", "data.table", "parallel", "pbmcapply")
+
+# library(CloudGeometry)
 library(lasR)
+## calcolo feature geometriche ----
 
 dGeom <- function(chunk, rgbmap) # user defined function
 {
@@ -18,90 +20,85 @@ dGeom <- function(chunk, rgbmap) # user defined function
   return(las) # output
 }
 
-file <- "data/cloud_merged2.laz"
-ctg <- readLAScatalog(file)
-opt_chunk_size(ctg) <- 100
-opt_chunk_buffer(ctg) <- 3
-opt_output_files(ctg) <- paste0("data/{ID}_geom")
-
-
+# file <- "data/cloud_merged2.laz"
+# ctg <- readLAScatalog(file)
+# opt_chunk_size(ctg) <- 100
+# opt_chunk_buffer(ctg) <- 3
+# opt_output_files(ctg) <- paste0("data/{ID}_geom")
 # output <- catalog_apply(ctg, dGeom )
-# library(rlang)
-read_las <- function(f, nn)
+
+getExtraMatrix <- function(chunk)
 {
-  read <- reader()
-  f2 <- function(...) {
-    args <- list(...)
+  # las <- readLAS(chunk$f)
+  # if (is.empty(las))  return(NULL)
 
-    ss <- sapply(args, mean)
-    ss2 <- as.list(ss)
-    names(ss2) <- nn
-    return( ss2)
+  area <- sf::st_buffer(chunk$p, -3 )
+  bb <- sf::st_bbox(area)
+  load <-  function(data) { return(data) }
+  read <- reader_rectangles(xmin = bb[["xmin"]],
+                            xmax = bb[["xmax"]],
+                            ymin = bb[["ymin"]],
+                            ymax = bb[["ymax"]]
+                            )
+  # read <- reader( )
+  calle <- callback(load, expose = "E", no_las_update = TRUE)
+
+  ee <- tryCatch({
+    exec(read + calle, on = chunk$f )
+  }, error = function(e) {
+    list(file = basename(chunk$f), error = e$message)  # ritorna errore e file
+  })
+
+  if(!is.data.frame(ee) || ncol(ee)==0 || nrow(ee)==0) return(list(file = basename(chunk$f), error = "No rows or cols"))
+
+  dt <- as.data.table(ee)
+  max_rows <- 1e6
+  dt_sampled <- if (nrow(dt) > max_rows) {
+    dt[sample(.N, max_rows)]
+  } else {
+    dt
   }
-  # create the rasterization step with the dynamically constructed function
-  callr <- rasterize(1, f2(r1.00_pointDensity,r1.00_eigenValue1,r1.00_eigenValue2,r1.00_eigenValue3,r1.00_eigenEntropy,r1.00_eigenSum,r1.00_PCA1,r1.00_PCA2,r1.00_linearity,r1.00_planarity,r1.00_sphericity,r1.00_verticality,r1.00_anisotropy,r1.00_omnivariance,r1.00_change_of_curvature,r0.50_pointDensity,r0.50_eigenValue1,r0.50_eigenValue2,r0.50_eigenValue3,r0.50_eigenEntropy,r0.50_eigenSum,r0.50_PCA1,r0.50_PCA2,r0.50_linearity,r0.50_planarity,r0.50_sphericity,r0.50_verticality,r0.50_anisotropy,r0.50_omnivariance,r0.50_change_of_curvature))
-
-  return (exec(read + callr, on = f))
+  return ( dt_sampled )
 }
 
-vals <- lidR::readLASheader(output[[1]])
+## estratto valori delle feature ----
+ff <- list.files("data/", pattern="\\.las$", full.names = T)
+ctg2 <- readLAScatalog( ff )
+opt_chunk_size(ctg2) <- 0
+opt_chunk_buffer(ctg2) <- -3
 
-nm <- names(vals$Extra_Bytes$`Extra Bytes Description`)
-cat( paste(nm, collapse=","), file="nma")
-las <- read_las(output[[1]], nm)
-plot(las)
+vv <- apply(ctg2@data , 1, function(x){ list(f=x[["filename"]], p=x[["geometry"]]) })
 
-f2 <- function(colname) {
-  colsym <- sym(colname)
-  function(df) {
-    mean(colsym)
-  }
+output2 <-  pbmclapply(vv, getExtraMatrix, mc.cores = 12)
+gc()
+
+output3 <- output2[sapply(output2, function(x){ class(x)=="data.frame" } )]
+all <- data.table::rbindlist(output3)
+rm(output2)
+rm(output3)
+gc()
+
+# write_fst(all, "all.fst")
+
+## MAD e statistiche per normalizzare ----
+getStats <- function(v){
+  message(length(v))
+  v <- na.omit(v)
+  v2 <- sample(v, min(length(v),10e6))
+  q2 <- quantile(v2, c(0.1,0.25,0.5, 0.75, 0.9)  )
+  return(list(mad=mad(v2),
+              mean=mean(v2),
+              q01=q[[1]], q25=q[[2]],
+              q50=q[[3]], q75=q[[4]],
+              q90=q[[5]] ) )
 }
-# column name you want to use
-colname <- "Intensity"
 
-# create the rasterization step with the dynamically constructed function
-call <- rasterize(20, f2(colname))
+## MAD e statistiche per normalizzare ----
+getStats <- function(v){
+  v <- na.omit(v)
+  q <- quantile(v, c(0.1,0.25,0.5, 0.75, 0.9),na.rm=T )
+  browser()
+  return(list(mad=mad(v,na.rm=T), mean=mean(v,na.rm=T), q01=q[[1]], q25=q[[2]], q50=q[[3]], q75=q[[4]], q90=q[[5]] ))
+}
+output4 <-  mclapply(all[1:1000,1:3], getStats, mc.cores = 1)
 
-# class = classify_with_csf(FALSE, 1 ,0.1, rigidness = 2) +
-#   write_las(ofile = "data/tempfiles/*.las"  ) +
-#   write_lax()
-#
-# tri  <- triangulate(filter = keep_ground())
-# dtm  <- rasterize(1, tri) # input is a triangulation stage
-# # norm <-  normalize() +  write_las(ofile = "data/tempfiles/norm*.las"  )
-# pipeline <- class + tri + dtm #+ norm
-# ans2 = exec(pipeline, on = ctg, progress = TRUE, ncores=64)
-#
-# ans2 <- list.files("data/tempfiles/")
-# ctg2 <- readLAScatalog(ans2)
-# opt_chunk_size(ctg2) <- 0
-# opt_chunk_buffer(ctg2) <- 3
-# opt_output_files(ctg) <- paste0("data/{ID}_")
-
-# chm1 <- rasterize(1, "max")
-# chm2 <- rasterize(1, "min", filter = "Classification == 2" )
-
-# ans <- exec(pipeline, on = ctg2,  with = list(progress = TRUE,  ncores=64))
-
-terra::writeRaster(ans2$rasterize, "data/dtm.tif", overwrite=T)
-# terra::writeRaster(ans[[1]], "data/max.tif", overwrite=T)
-# rlas::writelax(file)
-
-
-# lidR::index(ctg)
-# Step 2: Define a bounding box for spatial filtering
-
-las <- lidR::clip_circle(ctg,  288900, 5091400, 10)
-
-opt_output_files(las) <- ""
-opt_chunk_size(las) <- 0
-opt_chunk_buffer(las) <- 0
-
-output <- catalog_apply(las, dGeom )
-writeLAS( output[[1]], "data/output.laz")
-plot(ctg, chunk = TRUE, mapview=T)
-
-
-attributes <- ("data/25_geom.las")
-names(attributes@header$Extra_Bytes$`Extra Bytes Description`)
