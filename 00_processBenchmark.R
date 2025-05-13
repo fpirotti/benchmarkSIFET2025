3df667b1f8efd1c4b54a1b2bce56567df4d28551
fpirotti <francesco.pirotti@unipd.it>
2025-05-13 19:35:03 +0000

    update

--- 00_processBenchmark.R
+++ 00_processBenchmark.R
@@ -1,4 +1,4 @@
-pacman::p_load("lidR", "data.table", "parallel", "pbmcapply")
+pacman::p_load("lidR", "data.table", "h2o", "parallel", "pbmcapply")
 
 # library(CloudGeometry)
 library(lasR)
@@ -70,22 +70,23 @@ opt_chunk_buffer(ctg2) <- -3
 vv <- apply(ctg2@data , 1, function(x){ list(f=x[["filename"]], p=x[["geometry"]]) })
 
 output2 <-  pbmclapply(vv, getExtraMatrix, mc.cores = 12)
-gc()
+parallel::mcparallel(NULL)  # should return NULL
+gc()                        # force garbage collection
 
-output3 <- output2[sapply(output2, function(x){ class(x)=="data.frame" } )]
+
+output3 <- output2[sapply(output2, function(x){ class(x[[1]])=="numeric" } )]
 all <- data.table::rbindlist(output3)
 rm(output2)
 rm(output3)
 gc()
 
-# write_fst(all, "all.fst")
 
 ## MAD e statistiche per normalizzare ----
 getStats <- function(v){
-  message(length(v))
+  # message(length(v))
   v <- na.omit(v)
   v2 <- sample(v, min(length(v),10e6))
-  q2 <- quantile(v2, c(0.1,0.25,0.5, 0.75, 0.9)  )
+  q <- quantile(v2, c(0.1,0.25,0.5, 0.75, 0.9)  )
   return(list(mad=mad(v2),
               mean=mean(v2),
               q01=q[[1]], q25=q[[2]],
@@ -93,12 +94,73 @@ getStats <- function(v){
               q90=q[[5]] ) )
 }
 
-## MAD e statistiche per normalizzare ----
-getStats <- function(v){
-  v <- na.omit(v)
-  q <- quantile(v, c(0.1,0.25,0.5, 0.75, 0.9),na.rm=T )
-  browser()
-  return(list(mad=mad(v,na.rm=T), mean=mean(v,na.rm=T), q01=q[[1]], q25=q[[2]], q50=q[[3]], q75=q[[4]], q90=q[[5]] ))
-}
-output4 <-  mclapply(all[1:1000,1:3], getStats, mc.cores = 1)
+## apply loop for stats ------
+output4 <-  pbmclapply(all, getStats, mc.cores = 10)
+saveRDS(output4, "output4.rds")
+output4 <- readRDS("output4.rds")
+stats <- data.table::rbindlist(output4)
+all2 <- sweep( sweep(all, 2, stats$q50, "-"), 2, stats$mad, "/")
+rm(all)
+gc()
 
+ss <- sample(1:nrow(all2),size=3e6, replace = F)
+all3 <- all2[ss,]
+
+## h2o model kmeans ----------
+h2o.init()
+# df <- h2o::as.h2o(all3)
+# h2o::h2o.save_frame(df, "threeMpoints.frame")
+
+df <- h2o.load_frame(basename("frames/threeMpoints.frame/all3_sid_a652_5"), dir = "frames/threeMpoints.frame")
+remCols <- -1*(grep("pointDensity", colnames(df)))
+# colnames( df[,remCols] )
+segModel3m <- h2o::h2o.kmeans(df[,remCols], k=24, nfolds=0)
+# h2o::h2o.saveModel(segModel3m, "models/3Mpts_kmeans10k")
+
+segModel3m <- h2o::h2o.loadModel("models/3Mpts_kmeans10k/KMeans_model_R_1747151150972_8")
+
+## loop tiles ------
+### and normalize with MAD and median and then kmeans
+for(chunk in vv){
+  message(chunk$f)
+
+  area <- sf::st_buffer(chunk$p, -3 )
+  bb <- sf::st_bbox(area)
+
+  load <-  function(data) {
+    if(ncol(data)<20 || nrow(data)<1){
+      return(data)
+    }
+    all2 <- sweep( sweep(data[,-31], 2, stats$q50, "-"), 2, stats$mad, "/")
+    # remCols <- (grep("pointDensity", colnames(all2)))
+
+    # all2[,remCols] <- NULL
+    dd <- as.h2o(all2)
+    clusters <- as.data.frame(h2o.predict(segModel3m, dd))
+    data$segment <- clusters$predict
+    return(data)
+    }
+  # read <- reader()
+  # # read <- reader( )
+  read <- reader_rectangles(xmin = bb[["xmin"]],
+                            xmax = bb[["xmax"]],
+                            ymin = bb[["ymin"]],
+                            ymax = bb[["ymax"]]
+  )
+  calle <-
+    add_extrabytes("char", "segment", "k-means cluster") +
+    callback(load, expose = "E", no_las_update = FALSE)
+
+  ee <- tryCatch({
+    exec(read + calle + write_las(ofile = "data/outputcluster/*.laz"), on = chunk$f )
+  }, error = function(e) {
+    list(file = basename(chunk$f), error = e$message)  # ritorna errore e file
+  })
+
+  # if(is.character(ee)){
+  #   system(sprintf("/archivio/software/PotreeConverter/build/PotreeConverter %s", ee) )
+  # }
+
+    message(ee)
+
+}
